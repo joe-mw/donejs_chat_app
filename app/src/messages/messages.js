07feebc9653e8212125284a4101c6e24bc3263c9
Mwa <joseph.mwangi01@gmail.com>
2018-07-23 01:47:32 +0300

    Add logic to pull messages from API and save new messages

--- app/src/messages/messages.js
+++ app/src/messages/messages.js
@@ -2,10 +2,33 @@ import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './messages.less';
 import view from './messages.stache';
+import Message from '../models/message';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    default: 'This is the chat-messages component'
+
+  // creating a new message
+  name: 'string',
+  body: 'string',
+
+  // submit button to create a new message
+  send(event){
+    event.preventDefault();
+
+    // use 'save' to create a post request to /api/messages with the payload
+    new Message({
+      name: this.name,
+      body: this.body
+    }).save().then((result) => {
+      
+      // empty the message textbox
+      this.body = '';
+    }).catch((err) => {
+      throw new Error(err.message);
+    });
+  },
+
+  get messagesPromise(){
+    return Message.getList({});
   }
 });
 
